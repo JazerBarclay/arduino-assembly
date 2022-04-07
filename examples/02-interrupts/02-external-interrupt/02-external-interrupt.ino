4e0cba4fd8cf98dbfe5f1e600db3ca4d9c6d2e96
Jazer <contact@jazer.co.uk>
2022-04-07 19:36:18 +0100

    Added external interrupt example

---
+++ examples/02-interrupts/02-external-interrupt/02-external-interrupt.ino
@@ -0,0 +1,15 @@
+/* 
+ * EXTERNAL INTERRUPT
+ * 
+ * In this example we turn the on-board LED on or off when we receive
+ * a rising edge signal on pin 2.
+ * 
+ * Rather than polling the pin every loop we can have the chip jump out
+ * of the loop when a specified signal is received on a dedicated pin
+ * and return to our main code once its completed.
+ * 
+ * All code will be in the associated ".S" file in the same folder
+ * Happy Coding!
+ * 
+ * - Jazer
+ */
