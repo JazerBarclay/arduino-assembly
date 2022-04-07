4f612b51458686a09c06a6408c9aaa7e13919908
Jazer <contact@jazer.co.uk>
2022-04-07 17:54:20 +0100

    Added port control example

---
+++ examples/01-basics/03-port-control/03-port-control.ino
@@ -0,0 +1,18 @@
+/* 
+ * PORT CONTROL
+ * 
+ * Like the led control example, we can control pin mode and
+ * set LEDs high or low by controlling a bit in registers.
+ * 
+ * In this example we control a collection of pins by affecting
+ * all bits in a given PORT.
+ * 
+ * To do this in C we would have to loop over each pin however
+ * since we can control the whole port at once, we can set a whole
+ * bunch of pins and change them all at once.
+ *  
+ * All code will be in the associated ".S" file in the same folder
+ * Happy Coding!
+ * 
+ * - Jazer
+ */
