6dba8b7a87d281a997055bc0194d35fad694911e
Jazer <contact@jazer.co.uk>
2022-04-07 19:44:13 +0100

    Added strobe example

---
+++ examples/01-basics/05-strobe/05-strobe.ino
@@ -0,0 +1,15 @@
+/* 
+ * STROBE
+ * 
+ * In this example we make a collections of LEDs light in a sequence
+ * matching a register we shift a bit in.
+ * 
+ * We use a bit shift operation and output the state of the register
+ * to 8 LEDs on PORTA comprised of digital pins 22-29 at the bottom 
+ * of the arduino mega board.
+ *  
+ * All code will be in the associated ".S" file in the same folder
+ * Happy Coding!
+ * 
+ * - Jazer
+ */
