cf66314fab7bba810e6186ba53b26c5839d1d89f
Jazer <contact@jazer.co.uk>
2022-04-07 17:54:42 +0100

    Added blink example

---
+++ examples/01-basics/04-blink/04-blink.ino
@@ -0,0 +1,14 @@
+/* 
+ * BLINK
+ * 
+ * In this example we flash a single LED (pin 13 on the mega) on and off
+ * with a 1 second delay between each change.
+ * 
+ * To do this we use a few new opcodes to call functions, create loops, 
+ * temporarily store values on the stack and decrement values in registers.
+ *  
+ * All code will be in the associated ".S" file in the same folder
+ * Happy Coding!
+ * 
+ * - Jazer
+ */
