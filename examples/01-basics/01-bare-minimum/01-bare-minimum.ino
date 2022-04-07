d2181937f49a2cdc5189cc10c38c9a125f271efa
Jazer <contact@jazer.co.uk>
2022-04-07 16:45:06 +0100

    Renamed project bare-minimum to boilerplate

--- examples/01-basics/01-bare-minimum/01-bare-minimum.ino
+++
@@ -1,5 +0,0 @@
-/* 
- * All code will be in the associated ".S" file in the same folder
- * Happy Coding!
- * 
- */
---
+++ examples/01-basics/01-boilerplate/01-boilerplate.ino
@@ -0,0 +1,22 @@
+/* 
+ * BOILERPLATE
+ * 
+ * This is a minimal example of getting started with
+ * arduino assembly
+ * 
+ * We have 2 different files for writing assembly.
+ * 
+ * We have this .ino file where our C program would exists. We keep
+ * this file so its easier to open our arduino project in the
+ * arduino IDE.
+ * 
+ * The second file has the same name as the .ino file however has 
+ * the .S extension. This is where our assembly code will reside.
+ * 
+ * Take a look at the other file and check out how the code is
+ * setup and laid out.
+ * 
+ * Happy Coding!
+ * 
+ * - Jazer
+ */
