d44e0609723ea1ce636601d359d8039a8003ce78
Jazer <contact@jazer.co.uk>
2022-04-21 12:51:20 +0100

    Added serial transmission example

---
+++ examples/02-serial/01-transmit/01-transmit.ino
@@ -0,0 +1,10 @@
+/* 
+ * SERIAL TRANSMITTION
+ * 
+ * Prints the character 'A' to the serial port.
+ * 
+ * All code will be in the associated ".S" file in the same folder
+ * Happy Coding!
+ * 
+ * - Jazer
+ */
